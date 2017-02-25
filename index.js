db1fab8f37a7b4b2e891974503430e3a4551e996
Jared Gutierrez <j.gutierrez1@berkeley.edu>
2017-02-24 23:42:52 -0800

    enabling functionality

--- index.js
+++ index.js
@@ -21,7 +21,12 @@ app.get('/', function(req, res) {
 })
 
 
+
+
 //Facebook
+
+
+token = "EAATZAJ36ZCgdsBAEZCgrF9bs76KEOAVgh8cJ6raksLOILGRnFZAam7PZCwH08RrWsNjQJg1Dz2fLaGviZC8NvarFDlGFrsromhOanmwmZCaEOuCyaf0he0W1ZCbwS1AFxx5IWydbTYm8ZBIQCAq8axn2BxsHoNyZA5BKFiRNnWfZB0rBQZDZD"
 
 app.get('/webhook/', function(req, res) {
 	if (req.query['hub.verify_token'] === "JaredGutierrez") {
@@ -30,8 +35,41 @@ app.get('/webhook/', function(req, res) {
 	res.send("Wrong token")
 })
 
+function sendText(sender, text) {
+	let messageData = {text: text}
+	request({
+		url: "https://graph.facebook.com/v2.6/me/messages",
+		qs: {access_token, token},
+		method: "POST",
+		json: {
+			receipt: {id: sender},
+			message: messageData
+		}
+	}, function(error, response, body){
+		if (error) {
+			console.log("sending error")
+		} else if (response.body.error) {
+			console.log("response body error")
+		}
+	})
+}
+
+
+app.post('/webhook/', function(req, res) {
+	let messaging_events = req.body.entry[0].messaging_events
+	for (let i = 0; i < messaging_events.length; i ++){
+		let event = messaging_events[i]
+		let sender = event.sender.id
+		if (event.message && event.message.text) {
+			let text = event.message.text
+			sendText(sender, "Text echo: " + text.substring(0,100))
+		}
+	}
+	res.sendStatus(200)
+})
+
 app.listen(app.get('port'), function() {
 	console.log("running: port")
 })
 
-var x = "EAATZAJ36ZCgdsBAEZCgrF9bs76KEOAVgh8cJ6raksLOILGRnFZAam7PZCwH08RrWsNjQJg1Dz2fLaGviZC8NvarFDlGFrsromhOanmwmZCaEOuCyaf0he0W1ZCbwS1AFxx5IWydbTYm8ZBIQCAq8axn2BxsHoNyZA5BKFiRNnWfZB0rBQZDZD"
+// var x = "EAATZAJ36ZCgdsBAEZCgrF9bs76KEOAVgh8cJ6raksLOILGRnFZAam7PZCwH08RrWsNjQJg1Dz2fLaGviZC8NvarFDlGFrsromhOanmwmZCaEOuCyaf0he0W1ZCbwS1AFxx5IWydbTYm8ZBIQCAq8axn2BxsHoNyZA5BKFiRNnWfZB0rBQZDZD"
